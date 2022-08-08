e1f3b86d3c849c4dd4fb9870003e1b62cffa97af
WillBarbosa1 <dsilverfox98@gmail.com>
2022-08-07 21:36:42 -0300

    feat: scss configured

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <el-row class="header__title">Title</el-row>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
